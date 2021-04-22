af814b852bfbaa75804d1f83c2304f8bcdf15cc2
Marlon (esolitos) Saglia <marlon@ramsalt.com>
2021-04-22 12:35:02 +0200

    Add support for symlinked env file

--- deploy.sh
+++ deploy.sh
@@ -12,6 +12,13 @@ if [[ "#${CUSTOM_DOCKERFILE:-}" != "#" ]]; then
   BUILD_OPTS="${BUILD_OPTS} --dockerfile ${CUSTOM_DOCKERFILE}"
 fi
 
+if [[ "#${CREATE_ENV_SYMLINK:-}" != "#" ]]; then
+  ENV_FILE_PATH="${ENV_FILEPATH}"
+  ENV_SYMLINK_PATH="${BUILD_PATH:-.}/.env"
+
+  ln -sf "$ENV_FILE_PATH"  "$ENV_SYMLINK_PATH"
+fi
+
 # Initialize 
 wodby ci init $INSTANCE_UUID
 wodby ci build $BUILD_OPTS
