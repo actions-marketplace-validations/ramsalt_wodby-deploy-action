bee274094bc0236f90d30cb062948beb64122c82
Marlon (esolitos) Saglia <marlon@ramsalt.com>
2021-08-19 09:09:49 +0200

    Update action to include provider and build URL

--- deploy.sh
+++ deploy.sh
@@ -19,8 +19,16 @@ if [[ "#${CREATE_ENV_SYMLINK:-}" != "#" ]]; then
   ln -sf "$ENV_FILE_PATH"  "$ENV_SYMLINK_PATH"
 fi
 
-# Initialize 
-wodby ci init $INSTANCE_UUID
+echo "Initialize Wodby CI..."
+wodby ci init $INSTANCE_UUID \
+  --provider "GitHub Actions" \
+  --url "$GITHUB_SERVER_URL/$GITHUB_REPOSITORY/actions/runs/$GITHUB_RUN_ID"
+
+echo "Build app containers..."
 wodby ci build $BUILD_OPTS
+
+echo "Pushing containers to registry..."
 wodby ci release
+
+echo "Trigger app deploy..."
 wodby ci deploy
